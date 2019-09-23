3d056785ab6cb87ff603a67a71f149ed4b3d5b71
Marvin Buss <marvin.buss@gmail.com>
2019-09-23 12:35:52 +0200

    updated model registration

--- aml_service/20-RegisterModel.py
+++ aml_service/20-RegisterModel.py
@@ -23,69 +23,79 @@ IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THE SOFTWARE CODE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 """
-import os, json, sys
-from azureml.core import Workspace
-from azureml.core import Run
-from azureml.core import Experiment
+import os, json, azureml.core
+from azureml.core import Workspace, Experiment, Run
 from azureml.core.model import Model
-
-from azureml.core.runconfig import RunConfiguration
 from azureml.core.authentication import AzureCliAuthentication
+
+# Load the JSON settings file and relevant section
+print("Loading settings")
+with open(os.path.join("aml_config", "settings.json")) as f:
+    settings = json.load(f)
+workspace_config_settings = settings["workspace"]["config"]
+deployment_settings = settings["deployment"]
+
+# Get details from Run
+print("Loading Run Details")
+with open(os.path.join("aml_config", "run_details.json")) as f:
+    run_details = json.load(f)
+
+# Get Workspace
+print("Loading Workspace")
 cli_auth = AzureCliAuthentication()
+ws = Workspace.from_config(path=workspace_config_settings["path"], auth=cli_auth, _file_name=workspace_config_settings["file_name"])
+print(ws.name, ws.resource_group, ws.location, ws.subscription_id, sep = '\n')
 
-# Get workspace
-ws = Workspace.from_config(auth=cli_auth)
+# Loading Run
+print("Loading Run")
+experiment = Experiment(workspace=ws, name=run_details["experiment_name"])
+run = Run(experiment=experiment, run_id=run_details["run_id"])
 
-# Get the latest evaluation result
+# Only register model, if it performs better than the production model
+print("Register model only if it performs better.")
 try:
-    with open("aml_config/run_id.json") as f:
-        config = json.load(f)
-    if not config["run_id"]:
-        raise Exception("No new model to register as production model perform better")
+    # Loading run of production model
+    print("Loading Run of Production Model to evaluate new model")
+    production_model = Model(ws, name=deployment_settings["model"]["name"])
+    production_model_run_id = production_model.tags.get(["run_id"])
+    production_model_run = Run(experiment=experiment, run_id=production_model_run_id)
+
+    # Comparing models
+    print("Comparing Metrics of production and newly trained model")
+    promote_new_model = True
+    for metric in deployment_settings["model"]["evaluation_parameters"]["larger_is_better"]:
+        if not promote_new_model:
+            break
+        new_model_parameter = run.get_metrics().get(metric)
+        production_model_parameter = production_model_run.get_metrics().get(metric)
+        if new_model_parameter < production_model_parameter:
+            promote_new_model = False
+    for metric in deployment_settings["model"]["evaluation_parameters"]["smaller_is_better"]:
+        if not promote_new_model:
+            break
+        new_model_parameter = run.get_metrics().get(metric)
+        production_model_parameter = production_model_run.get_metrics().get(metric)
+        if new_model_parameter > production_model_parameter:
+            promote_new_model = False
+
+    if promote_new_model:
+        print("New model performs better, thus it will be registered")
+    else:
+        print("New model does not perform better.")
 except:
-    print("No new model to register as production model perform better")
-    # raise Exception('No new model to register as production model perform better')
-    sys.exit(0)
+    promote_new_model = True
+    print("This is the first model to be trained, thus nothing to evaluate for now")
 
-run_id = config["run_id"]
-experiment_name = config["experiment_name"]
-exp = Experiment(workspace=ws, name=experiment_name)
-
-run = Run(experiment=exp, run_id=run_id)
-names = run.get_file_names
-names()
-print("Run ID for last run: {}".format(run_id))
-model_local_dir = "model"
-os.makedirs(model_local_dir, exist_ok=True)
-
-# Download Model to Project root directory
-model_name = "sklearn_regression_model.pkl"
-run.download_file(
-    name="./outputs/" + model_name, output_file_path="./model/" + model_name
-)
-print("Downloaded model {} to Project root directory".format(model_name))
-os.chdir("./model")
-model = Model.register(
-    model_path=model_name,  # this points to a local file
-    model_name=model_name,  # this is the name the model is registered as
-    tags={"area": "diabetes", "type": "regression", "run_id": run_id},
-    description="Regression model for diabetes dataset",
-    workspace=ws,
-)
-os.chdir("..")
-print(
-    "Model registered: {} \nModel Description: {} \nModel Version: {}".format(
-        model.name, model.description, model.version
-    )
-)
-
-# Remove the evaluate.json as we no longer need it
-# os.remove("aml_config/evaluate.json")
-
-# Writing the registered model details to /aml_config/model.json
-model_json = {}
-model_json["model_name"] = model.name
-model_json["model_version"] = model.version
-model_json["run_id"] = run_id
-with open("aml_config/model.json", "w") as outfile:
-    json.dump(model_json, outfile)
+# Registering new Model
+if promote_new_model:
+    print("Registering new Model")
+    tags = deployment_settings["model"]["tags"]
+    tags = tags.update({"run_id": run.id})
+    model = run.register_model(model_name=deployment_settings["model"]["name"],
+                               model_path=deployment_settings["model"]["path"],
+                               tags=tags,
+                               properties=deployment_settings["model"]["properties"],
+                               model_framework=deployment_settings["model"]["model_framework"],
+                               model_framework_version=deployment_settings["model"]["model_framework_version"],
+                               description=deployment_settings["model"]["description"],
+                               datasets=deployment_settings["model"]["datasets"])
